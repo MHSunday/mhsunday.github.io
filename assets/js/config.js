bc30f97aafbd1d75d6123f82930843083a59fbff
MHSunday <angelina-sunday@vps.frleong.org>
2025-09-16 17:20:53 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,4 +5,5 @@ window.APP_CONFIG = {
   GOOGLE_CAL_EMBED: "https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FMacau&showPrint=0&src=c2Z4c3VuZGF5QGdtYWlsLmNvbQ&src=ZGYyNjZmMmNmNDBlYzIzMTE0ZjMxMTcwMDYwZjJkNWJlYTZhMzBlYTcxZTk2OGRkNTQ5NmUwN2E1ZWMzMWFiNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039be5&color=%233f51b5",
   OAUTH_CLIENT_ID: "339277687965-tvtrrvumejbgh9v4mj9v2nad2ftvmhjo.apps.googleusercontent.com",
   DEFAULT_CLASSES: ["幼兒班","聖體班(1)","聖體班(2)","聖體班(3)","聖體後班","堅振班(1A)","堅振班(1B)","堅振班(2)","堅振班(3)"]
+
 };
