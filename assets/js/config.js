19421e23a59632b5ec8d432c9fc37cdaa8f94861
Angelina <angelina-sunday@vps.frleong.org>
2025-09-17 10:13:48 +0800

    modified:   assets/js/config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 // assets/js/config.js
 // 全域設定（請依實際情況替換）
 window.APP_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbzR4ZVlvesmRR4dq_bdJAGHtZG9_ZcgpqD-MfPtRiG7ZC_iSmJ-mSjCsYkmTNuyFKY/exec",
+  API_BASE: "https://script.google.com/macros/s/AKfycbyPrbl5xLO_lQWr-1mUhOcS06PXhfesBRKgDmfD3yPLtw4besNtbZs264ekQKnynM8/exec",
   GOOGLE_CAL_EMBED: "https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FMacau&showPrint=0&src=c2Z4c3VuZGF5QGdtYWlsLmNvbQ&src=ZGYyNjZmMmNmNDBlYzIzMTE0ZjMxMTcwMDYwZjJkNWJlYTZhMzBlYTcxZTk2OGRkNTQ5NmUwN2E1ZWMzMWFiNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039be5&color=%233f51b5",
   OAUTH_CLIENT_ID: "339277687965-7gkbr7hi74iv2viu92flrfh65vm7ibt2.apps.googleusercontent.com",
   DEFAULT_CLASSES: ["幼兒班","聖體班(1)","聖體班(2)","聖體班(3)","聖體後班","堅振班(1A)","堅振班(1B)","堅振班(2)","堅振班(3)"]
